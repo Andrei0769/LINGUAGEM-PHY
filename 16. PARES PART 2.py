d8b7121ab2e740c15a9c585180a1980b9a0fe51a
Andrei0769 <159576658+Andrei0769@users.noreply.github.com>
2024-05-03 01:40:17 +0000

    EXERCICIOS INICIAIS DE PYTHON

--- 16. PARES PART 2.py
+++ 16. PARES PART 2.py
@@ -1,20 +1,19 @@
 import os
 
+# Limpar a tela do console
 os.system("cls || clear")
+
 pares = 0
 impares = 0
 
-
 for i in range(1, 6):
-    num = int (input(f"Digite o {i}º Número: "))
+    num = int(input(f"Digite o {i}º Número: "))
 
-    if i %2==0:
+    if num % 2 == 0:
         pares += 1
-            
-    else: 
+    else:
         impares += 1
 
 print(f"Pares: {pares}")
-print(f"Impares: {impares}")
+print(f"Ímpares: {impares}")
 
-            
