8b810b940321bfdcdaeda4a51a4d54de485de007
Andrei0769 <159576658+Andrei0769@users.noreply.github.com>
2024-06-16 23:15:59 -0300

    Update abrir_paginas.py

--- 4. ESTRUTURA DE DADOS.py/abrir_paginas.py
+++ 4. ESTRUTURA DE DADOS.py/abrir_paginas.py
@@ -1,25 +1,30 @@
+import subprocess
 import webbrowser
 import os
+import time
 
 def abrir_whatsapp():
     # Abrir WhatsApp Web em uma janela anônima
-    webbrowser.get('windows-default').open_new_tab('https://web.whatsapp.com', autoraise=True)
+    subprocess.Popen(['start', 'chrome', '--incognito', 'https://web.whatsapp.com'])
 
 def abrir_email():
     # Abrir email em uma janela normal
-    webbrowser.open_new_tab('https://mail.google.com', autoraise=True)
+    subprocess.Popen(['start', 'chrome', 'https://mail.google.com'])
 
 def abrir_github():
     # Abrir GitHub em uma janela normal
-    webbrowser.open_new_tab('https://github.com', autoraise=True)
+    subprocess.Popen(['start', 'chrome', 'https://github.com'])
 
 if __name__ == "__main__":
     # Abrir WhatsApp Web e email em uma tela (monitor primário)
     abrir_whatsapp()
     abrir_email()
 
+    # Pausa para permitir que as janelas sejam abertas
+    time.sleep(2)
+
     # Abrir GitHub em outra tela (monitor secundário)
     abrir_github()
 
-    # Pausa para manter as janelas abertas (opcional)
+    # Pausa opcional para manter as janelas abertas (opcional)
     input("Pressione Enter para fechar o programa...")
