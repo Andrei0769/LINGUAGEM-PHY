da3148385b657c4fd520254021623e87d940d50a
Andrei0769 <159576658+Andrei0769@users.noreply.github.com>
2024-06-16 23:26:10 -0300

    Update abrir_paginas.py

--- 4. ESTRUTURA DE DADOS.py/abrir_paginas.py
+++ 4. ESTRUTURA DE DADOS.py/abrir_paginas.py
@@ -1,19 +1,17 @@
-import subprocess
-import webbrowser
 import os
 import time
 
 def abrir_whatsapp():
     # Abrir WhatsApp Web em uma janela anônima
-    subprocess.Popen(['start', 'chrome', '--incognito', 'https://web.whatsapp.com'])
+    os.startfile('https://web.whatsapp.com')
 
 def abrir_email():
     # Abrir email em uma janela normal
-    subprocess.Popen(['start', 'chrome', 'https://mail.google.com'])
+    os.startfile('https://mail.google.com')
 
 def abrir_github():
     # Abrir GitHub em uma janela normal
-    subprocess.Popen(['start', 'chrome', 'https://github.com'])
+    os.startfile('https://github.com')
 
 if __name__ == "__main__":
     # Abrir WhatsApp Web e email em uma tela (monitor primário)
